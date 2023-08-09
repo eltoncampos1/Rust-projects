38a16754e48032572bb8ed135c8571bd302cc1cc
Elton Campos <eltoncampos36@gmail.com>
2023-08-09 01:36:54 -0300

    feat: create minigrep cli

---
+++ minigrep/src/main.rs
@@ -0,0 +1,42 @@
+use std::env;
+use std::fs;
+use std::process;
+use std::error::Error;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+
+    println!("Contents: {}", contents);
+    Ok(())
+}
+
+struct Config {
+    query: String,
+    filename: String
+}
+
+impl Config {
+    fn new(args: &Vec<String>) -> Result<Self, &str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments.");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }   
+}
+
