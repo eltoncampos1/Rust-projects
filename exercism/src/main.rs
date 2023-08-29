532fdf1e6bf16839efb56975643a692e983e7ace
Elton Campos <eltoncampos36@gmail.com>
2023-08-29 14:10:20 -0300

    feat: exercism, ONE_GIGA

--- exercism/src/main.rs
+++ exercism/src/main.rs
@@ -1,8 +1,16 @@
+use time::{Duration, PrimitiveDateTime as DateTime};
+
+
 fn main() {
     println!("Exercism");
 }
 
 pub fn reverse(input: &str) -> String {
     input.chars().rev().collect()
- }
+}
+
+const ONE_GIGA: i64 = 1000000000;
+pub fn after(start: DateTime) -> DateTime {
+    start + Duration::seconds(ONE_GIGA)
+}
  
